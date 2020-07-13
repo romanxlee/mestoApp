4eeb926a9588340ca53fea7d3cfd6bb799c3eaa0
Roman Lee <romanxlee@yandex.ru>
2020-07-13 13:53:06 +0300

    Добалвена сборка вэбпаком

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # mestoApp
-Mesto
---
+++ src/js/Popup.js
@@ -0,0 +1,14 @@
+export default class Popup {
+    constructor(container) {
+        this.container = container;
+    }
+
+    open() {
+        this.container.classList.add('popup_is-opened');
+    }
+
+    close() {
+        this.container.classList.remove('popup_is-opened');
+    }
+}
+
